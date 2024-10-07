e40c2fbbdf78605cb69aa797c5e8d298420a95f8
IsabelVliegen <161309209+IsabelVliegen@users.noreply.github.com>
2024-10-07 15:55:26 +0200

    Added (General) Controller (Specify for Agenda?)

---
+++ VictuZ_2.0_API/Controllers/VictuZ_2.0_API_Controller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VictuZ_2._0_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VictuZ_2 : ControllerBase
+    {
+        // GET: api/<VictuZ_2>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<VictuZ_2>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<VictuZ_2>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VictuZ_2>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VictuZ_2>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
